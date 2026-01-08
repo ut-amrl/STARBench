2912470bd10747b6aaa68794a37c6552a149b0a0
TieJean <taijingchen99@gmail.com>
2026-01-08 12:53:50 -0600

    update data download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+wget -O starbench_tasks.zip -L https://utexas.box.com/shared/static/o5lg2ysjk7n4rggj5rpzzzkezk54tho5.zip
+unzip starbench_tasks.zip
+wget -O startbench_data.zip -L https://utexas.box.com/shared/static/huy3ljgd1legpnjdvgtnvsl77fifyp2e.zip
+unzip startbench_data.zip
